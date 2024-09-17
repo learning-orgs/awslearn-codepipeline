76451aae45bc6559a31b57fed6494f52d39f13d7
Manoj Patidar <manojpatidar0505@yahoo.com>
2024-09-17 18:01:30 +0530

    first commit

---
+++ src/main/java/com/aws/awslearncodepipeline/AwslearnCodepipelineApplication.java
@@ -0,0 +1,13 @@
+package com.aws.awslearncodepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwslearnCodepipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwslearnCodepipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aws/awslearncodepipeline/AwslearnCodepipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.awslearncodepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwslearnCodepipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
